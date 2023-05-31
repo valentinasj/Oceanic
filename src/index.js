b3f7c434a5a97a0afddc8e58f3ba84069130c2f4
Andrés Parra <pipe.parrab@gmail.com>
2023-05-31 00:07:45 -0500

    Añadiendo etiqueta React.StrictMode en el index.js.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss'
-import AppRouter from './routes/AppRouter';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import AppRouter from "./routes/AppRouter";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( 
-  <AppRouter/>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <AppRouter />
+  </React.StrictMode>
 );
